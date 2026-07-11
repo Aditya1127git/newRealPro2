a44e84af9977e47c874ef592053dbb8053db1bf7
Aditya <soubhagyalaxmis29@gmail.com>
2026-07-12 00:34:07 +0530

    updated

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const cors = require("cors");
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
 
 mongoose.connect("mongodb+srv://adityasatapathy1127_db_user:V26HooPWPWrVY6jy@cluster0.6rwo4zd.mongodb.net/vtu?retryWrites=true&w=majority&appName=Cluster0")
